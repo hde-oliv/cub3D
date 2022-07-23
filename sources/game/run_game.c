e4d637104df76779690854b4d8177f41c65629d4
hde-oliv <henriquerocha@tutanota.com>
2022-07-23 19:38:57 -0300

    fix: call right function on run_game

--- sources/game/run_game.c
+++ sources/game/run_game.c
@@ -6,17 +6,16 @@
 /*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 19:38:17 by hde-oliv          #+#    #+#             */
-/*   Updated: 2022/07/23 18:09:47 by hde-oliv         ###   ########.fr       */
+/*   Updated: 2022/07/23 18:26:08 by hde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-
 void	run_game(t_game *game)
 {
 	set_view(game);
-	run_hooks(game);
+	set_hooks(game);
 }
 
 void	set_view(t_game *game)
