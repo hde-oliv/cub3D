9898b4f6611abfe6167c4ea06534172838f7b465
hde-oliv <henriquerocha@tutanota.com>
2022-08-01 19:30:59 -0300

    fix: right and left movement

--- sources/game/movement.c
+++ sources/game/movement.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   movement.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snovaes <snovaes@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 18:17:14 by hde-oliv          #+#    #+#             */
-/*   Updated: 2022/07/31 23:25:16 by snovaes          ###   ########.fr       */
+/*   Updated: 2022/08/01 19:10:00 by hde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,20 +40,26 @@ void	move_player_backward(t_game *game)
 
 void	move_player_right(t_game *game)
 {
-	if (game->player.x - game->direction.y * game->move_speed > 1 && \
-		game->player.x - game->direction.y * game->move_speed)
+	int	next_x;
+	int	next_y;
+
+	next_x = game->player.x - game->direction.y * game->move_speed;
+	next_y = game->player.y + game->direction.x * game->move_speed;
+	if ((game->map->rows)[next_x][(int)game->player.y] == 0)
 		game->player.x -= game->direction.y * game->move_speed;
-	if (game->player.y + game->direction.x * game->move_speed > 1 && \
-		game->player.y + game->direction.x * game->move_speed)
+	if ((game->map->rows)[(int)game->player.x][next_y] == 0)
 		game->player.y += game->direction.x * game->move_speed;
 }
 
 void	move_player_left(t_game *game)
 {
-	if (game->player.x - game->direction.y * game->move_speed > 1 && \
-		game->player.x - game->direction.y * game->move_speed)
+	int	next_x;
+	int	next_y;
+
+	next_x = game->player.x + game->direction.y * game->move_speed;
+	next_y = game->player.y - game->direction.x * game->move_speed;
+	if ((game->map->rows)[next_x][(int)game->player.y] == 0)
 		game->player.x += game->direction.y * game->move_speed;
-	if (game->player.y + game->direction.x * game->move_speed > 1 && \
-		game->player.y + game->direction.x * game->move_speed)
+	if ((game->map->rows)[(int)game->player.x][next_y] == 0)
 		game->player.y -= game->direction.x * game->move_speed;
 }
