24ef80d88c0229e99f6b30cae93736031b37458c
hde-oliv <henriquerocha@tutanota.com>
2022-08-01 19:30:49 -0300

    fix: leak when game ends

--- sources/game/end_game.c
+++ sources/game/end_game.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   end_game.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snovaes <snovaes@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 21:39:07 by hde-oliv          #+#    #+#             */
-/*   Updated: 2022/07/31 23:28:54 by snovaes          ###   ########.fr       */
+/*   Updated: 2022/08/01 19:22:12 by hde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,20 @@ int	end_game(void *p)
 	t_game	*game;
 
 	game = (t_game *)p;
+	mlx_clear_window(game->mlx, game->win);
+	mlx_destroy_image(game->mlx, game->screen->img);
+	mlx_destroy_image(game->mlx, game->n_sprite->img);
+	mlx_destroy_image(game->mlx, game->w_sprite->img);
+	mlx_destroy_image(game->mlx, game->s_sprite->img);
+	mlx_destroy_image(game->mlx, game->e_sprite->img);
 	mlx_destroy_window(game->mlx, game->win);
-	mlx_destroy_image(game->mlx, game->screen);
 	mlx_destroy_display(game->mlx);
+	free(game->screen);
+	free(game->n_sprite);
+	free(game->s_sprite);
+	free(game->w_sprite);
+	free(game->e_sprite);
 	free(game->mlx);
 	free_everything(game);
-	game->mlx = NULL;
 	exit(0);
 }
